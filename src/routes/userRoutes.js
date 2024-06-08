3db558302bfc639dd4a75f0b17036cfef225c6e0
Badarinath <67597010+Badarijitwta@users.noreply.github.com>
2024-06-08 22:48:35 +0530

    Changed Auth and folder structure

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
-import authMiddleware from './../middlewares/authmiddleware';
+
 
 const router = express.Router();
 
@@ -13,6 +13,6 @@ router.post('/register', userController.registerUser);
 // User login
 router.post('/login', userController.loginUser);
 
-router.get('/', authMiddleware, userController.getAllUsers)
+router.get('/', userController.getAllUsers)
 
 export default router;
